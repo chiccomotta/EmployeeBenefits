2a740fc551b1b77aad0fd9d90c8b4973fea6ca61
cristiano motta <cristiano.motta@hotmail.it>
2019-01-22 22:57:23 +0100

    test connection

--- EmployeeBenefits.Test/TestClass.cs
+++ EmployeeBenefits.Test/TestClass.cs
@@ -1,22 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.IO;
 using Xunit;
+using Environment = NHibernate.Cfg.Environment;
 
 namespace Unit.Test
 {
-    public class TestClass
+    public class InMemoryDatabaseForXmlMappings : IDisposable
     {
-        [Theory]
-        [InlineData(2, 3)]
-        [InlineData(1, 4)]
-        [InlineData(0, 5)]
-        public void Somma(int x, int y)
+        protected Configuration config;
+        protected ISessionFactory sessionFactory;
+        public ISession Session;
+
+        [Fact]
+        public void InMemoryDatabaseXmlMappings()
+        {            
+            config = new Configuration()
+                .SetProperty(Environment.ReleaseConnections, "on_close")
+                .SetProperty(Environment.Dialect, typeof (SQLiteDialect).AssemblyQualifiedName)
+                .SetProperty(Environment.ConnectionDriver, typeof (SQLite20Driver).AssemblyQualifiedName)
+                .SetProperty(Environment.ConnectionString, "data source =:memory:")
+                .AddFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                    @"..\..\..\..\Persistence/bin/debug/netcoreapp2.1/Xml/Employee.hbm.xml"));
+
+            sessionFactory = config.BuildSessionFactory();
+
+            Session = sessionFactory.OpenSession();
+
+            new SchemaExport(config).Execute(
+                useStdOut: true,
+                execute: true,
+                justDrop: false,
+                connection: Session.Connection,
+                exportOutput: Console.Out);
+        }
+
+        public void Dispose()
         {
-            var somma = x + y;
-            
-            //Assert
-            Assert.Equal(5, somma);
+            Session.Dispose();
+            sessionFactory.Dispose();
         }
     }
 }
